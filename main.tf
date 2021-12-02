7ef4b73a5fc11dbd2e4b4f6b185511a5b6dc7122
lot8229 <lot8229@kakao.com>
2021-12-02 12:23:19 +0900

    local Data Sources Handling

--- main.tf
+++ main.tf
@@ -2,7 +2,14 @@ provider "local" {
   # Configuration options
 }
 resource "local_file" "foo" {
-    content     = "foo!"
-    filename = "${path.module}/foo.bar"
+    filename = "${path.module}/foo.txt"
+    content     = "Hello World!"
+
 }
 
+data "local_file" "bar" {
+    filename = "${path.module}/bar.txt"
+}
+output "file_bar" {
+  value = data.local_file.bar
+} 
