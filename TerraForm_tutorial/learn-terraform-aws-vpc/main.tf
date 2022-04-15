458f557b0e2448e5bad798e5a3e67c28938ff567
lot8229 <lot8229@kakao.com>
2022-04-15 18:30:48 +0900

    set NGW & IGW set route

--- TerraForm_tutorial/learn-terraform-aws-vpc/main.tf
+++ TerraForm_tutorial/learn-terraform-aws-vpc/main.tf
@@ -70,3 +70,31 @@ resource "aws_subnet" "priv2"{
     }
 }
 
+resource "aws_internet_gateway" "terraform-vpc-igw"{
+  vpc_id = "${aws.vpc.terraform-vpc.id}"
+  tags = {
+    "Name" = "terraform-igw"
+  }
+}
+
+resource "aws_route" "internet-access"{
+  route_table_id = "${aws_vpc.terraform-vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internetgateway.terraform-vpc.id}"
+}
+
+resource "aws_eip" "terraform-vpc-eip" {
+  vpc = true
+  depends_on = ["aws_internet_gateway.terraform-vpc-igw"]
+}
+resource "aws_nat_gateway" "terraform-vpc-ngw" {
+  allocation_id = aws_eip.terraform-vpc-eip.id
+  tags = {
+    "Name" = "igw"
+  }
+}
+resource "aws_route" "internet_access" {
+  route_table_id = "${aws_vpc.terraform-vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.terraform-vpc-igw.id}"
+}
