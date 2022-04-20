b86166ce840d9c1fb68f10d2eff1c746c63c276c
lot8229 <lot8229@kakao.com>
2022-04-20 17:01:08 +0900

    default network acl

--- TerraForm_tutorial/learn-terraform-aws-vpc/main.tf
+++ TerraForm_tutorial/learn-terraform-aws-vpc/main.tf
@@ -162,3 +162,26 @@ resource "aws_default_security_group" "terraform-default" {
     "Name" = "Default"
   }
 }
+
+resource "aws_default_network_acl" "terraform-default" {
+  default_network_acl_id = aws_vpc.terraform-vpc.default_network_acl_id
+  ingress {
+    protocol = -1
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+  }
+  egress {
+    protocol = -1
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+  }
+  tags = {
+    "Name" = "default"
+  }
+}
