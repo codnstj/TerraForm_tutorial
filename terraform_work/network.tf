dde983c2ff345f44271d7adc179e66caa8a227bb
lot8229 <lot8229@kakao.com>
2022-08-26 17:53:44 +0900

    networks test

--- terraform_work/network.tf
+++ terraform_work/network.tf
@@ -8,7 +8,7 @@ resource "aws_subnet" "ecs_subnet_pub" {
   vpc_id = aws_vpc.ecs_cluster_vpc
   count = 2
   cidr_block = "10.30.${10 + count.index}.0/24"
-  availability_zone = "${data.aws_availbility_zones.available.names[0]}"
+  availability_zone = "ap-northeast-2a"
   tags = {
     "Name" = "ecs_subnet_${1 + count.index}"
   }
@@ -18,13 +18,12 @@ resource "aws_subnet" "ecs_subnet_priv" {
   vpc_id = aws_vpc.ecs_cluster_vpc.id
   count = 2
   cidr_block = "10.30.${12 + count.index}.0/24"
-  availability_zone = "${data.aws_availbility_zones.available.names[2]}"
+  availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "ecs_subnet_${2 + count.index}"
   }
 }
-
-resource "aws_internet_gateway" "ecs_igw" {
+ resource "aws_internet_gateway" "ecs_igw" {
   vpc_id = aws_vpc.ecs_cluster_vpc.id
   tags = {
     "Name" = "ecs_igw"
@@ -41,7 +40,7 @@ resource "aws_eip" "nat_gw" {
 resource "aws_nat_gateway" "ecs_ngw" {
   allocation_id = aws_eip.nat_gw.id
   connectivity_type = "private"
-  subnet_id = aws_subnet.ecs_subnet_priv.id
+  subnet_id = element(aws_subnet.ecs_subnet_pub.*.id ,0)
 }
 
 resource "aws_route_table" "priv_route" {
@@ -55,22 +54,21 @@ resource "aws_route_table" "priv_route" {
   }  
 }
 resource "aws_default_route_table" "default_route" {
-  default_route_table_id = "${aws_vpc.side_effect.default_route_table_id}"
+  default_route_table_id = "${aws_vpc.ecs_cluster_vpc.default_route_table_id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ecs_igw.id
   }
-  tags {
-    Name = "public_route_table"
-  }
 }
 
 resource "aws_route_table_association" "public_subnets_association" {
-  subnet_id = aws_subnet.ecs_subnet_pub.id
+  count = length(aws_subnet.ecs_subnet_pub.*.id)
+  subnet_id = element(aws_subnet.ecs_subnet_pub.*.id, count.index)
   route_table_id = aws_default_route_table.default_route.id
 }
 
 resource "aws_route_table_association" "private_subnets_association" {
-  subnet_id = aws_subnet.ecs_subnet_priv.id
+  count = length(aws_subnet.ecs_subnet_priv.*.id)
+  subnet_id = element(aws_subnet.ecs_subnet_priv.*.id, count.index)
   route_table_id = aws_route_table.priv_route.id
 }
